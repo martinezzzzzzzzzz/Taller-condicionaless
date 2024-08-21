a431b1393bb16be0bc9e2dc44d3eb0282849f233
Emanuel <martinezvalencia1200@gmail.com>
2024-08-21 14:20:49 -0500

    Ejercicio 2

---
+++ script.js
@@ -0,0 +1,20 @@
+//EJERCICIO 1
+let edad = prompt("Ingrese su edad: "); 
+if (edad >=18) {
+    console.log ("Eres mayor de edad ")
+
+}
+
+else {
+    console.log ("Eres menor de edad ")
+}
+
+//EJERCICIO 2
+
+let numeroUsuario = prompt ("Ingrese su número: ")
+if (numeroUsuario >=0) {
+    console.log("El número es positivo ")
+}
+else { 
+    console.log ("El número es negativo ")
+}
